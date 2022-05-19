2b07fb635f79880ececcbc49e35deff5f93dbc39
Yurik <yurik522@gmail.com>
2022-05-19 09:50:29 +0300

    Work code, v2

--- Program.cs
+++ Program.cs
@@ -2,22 +2,20 @@
                              "возможность.", "Так", "что", "не", "теряй", "времени,", "выбирай", 
                              "слова", "и", "не", "упускай", "возможности"};
 Console.WriteLine(array.Length);
-// foreach (str s in array)
-// {
-//    for(int i = 0; i<array.Length |; i) 
-// }
+
 int j = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if(array[i].Length <= 3) j++;        
-}
-string[] newArray = new string[j];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if(array[i].Length <= 3) j++;        
+// }
+string[] newArray = new string[0];
 Console.WriteLine(j);
 j = 0;
 for (int i = 0; i < array.Length; i++)
 {
     if(array[i].Length <= 3) 
     {
+        Array.Resize(ref newArray, 1 + j);
         newArray[j] = array[i];
         Console.Write($"\"{newArray[j]}\" ");
         j++;
