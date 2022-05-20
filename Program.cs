8cf8d888d6f0eba159a60290884df058eee872ba
Yurik <yurik522@gmail.com>
2022-05-20 18:28:40 +0300

    Create a block-scheme

--- Program.cs
+++ Program.cs
@@ -1,39 +1,23 @@
-﻿// int SumRequiredElements(string[] collection, int stringLength)
-// {
-//     int j = 0;
-//     for (int i = 0; i < collection.Length; i++)
-//     {
-//         if (collection[i].Length <= 3)
-//            {
-//                 j++;
-//                 break;
-//            }
-//     }
-//     return j;
-// }
-bool FindElements(string[] collection, int stringLength)
+﻿bool FindElements(string[] collection, int stringLength)
 {
     bool res = false;
-        for (int i = 0; i < collection.Length; i++)
+    for (int i = 0; i < collection.Length; i++)
     {
-        if (collection[i].Length <= 3)
-           {
-                j++;
-                break;
-           }
+        if (collection[i].Length <= stringLength)
+        {
+            res = true;
+        }
     }
-    return j;
+    return res;
 }
 
-
-
-string[] CreateSortArray(string[] matrix)
+string[] CreateSortArray(string[] matrix, int length)
 {
     string[] newArray = new string[0];
     int j = 0;
     for (int i = 0; i < matrix.Length; i++)
     {
-        if (matrix[i].Length <= 1)
+        if (matrix[i].Length <= length)
         {
             Array.Resize(ref newArray, 1 + j);
             newArray[j] = matrix[i];
@@ -47,14 +31,18 @@ void PrintArray(string[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length - 1; i++)
     {
-        Console.Write("{0,4}",  $"\"{array[i]}\",");
+        Console.Write("{0,4}", $"\"{array[i]}\",");
     }
-    Console.WriteLine("{0,4}",  $"\"{array[array.Length - 1]}\"]");
+    Console.WriteLine("{0,4}", $"\"{array[array.Length - 1]}\"]");
 }
 string[] inputArray = new string[]{"Три", "вещи", "не", "возвращаются", "обратно", "-", "время", "слово", 
                              "возможность.", "Так", "что", "не", "теряй", "времени,", "выбирай", 
                              "слова", "и", "не", "упускай", "возможности"};
-                             
-int size = (SumRequiredElements(inputArray, 3));
-Console.WriteLine($"j = {size}");
-PrintArray(CreateSortArray(inputArray));
+int lengthElement = 3;
+if (FindElements(inputArray, lengthElement))
+{
+    PrintArray(CreateSortArray(inputArray, lengthElement));
+}
+else
+    Console.WriteLine("[]");
+
