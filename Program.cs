c4a4ab1f9cf30ba06eef292fffaa5c168154efa5
Yurik <yurik522@gmail.com>
2022-05-20 19:44:55 +0300

    Release version

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿bool FindElements(string[] collection, int stringLength)
-{
+﻿
+bool FindElements(string[] collection, int stringLength) //определяем наличие элементов, удовлетворяющих
+{                                                        //условию задачи
     bool res = false;
     for (int i = 0; i < collection.Length; i++)
     {
@@ -11,22 +12,24 @@
     return res;
 }
 
-string[] CreateSortArray(string[] matrix, int length)
-{
+string[] CreateNewArray(string[] arrayInput, int lengthFindElements) // создаём из элементов, удовлетворя-
+{                                                                    //ющих условию задачи новый массив
     string[] newArray = new string[0];
-    int j = 0;
-    for (int i = 0; i < matrix.Length; i++)
+    int len = arrayInput.Length;
+    int index = 0;
+    int newIndex = 0;
+    for (index = 0; index < len; index++)
     {
-        if (matrix[i].Length <= length)
+        if (arrayInput[index].Length <= lengthFindElements)
         {
-            Array.Resize(ref newArray, 1 + j);
-            newArray[j] = matrix[i];
-            j++;
+            Array.Resize(ref newArray, 1 + newIndex);
+            newArray[newIndex] = arrayInput[index];
+            newIndex++;
         }
     }
     return newArray;
 }
-void PrintArray(string[] array)
+void PrintArray(string[] array) //метод вывода массива в консоль
 {
     Console.Write("[");
     for (int i = 0; i < array.Length - 1; i++)
@@ -35,14 +38,22 @@ void PrintArray(string[] array)
     }
     Console.WriteLine("{0,4}", $"\"{array[array.Length - 1]}\"]");
 }
+//Задаём массив строк
 string[] inputArray = new string[]{"Три", "вещи", "не", "возвращаются", "обратно", "-", "время", "слово", 
                              "возможность.", "Так", "что", "не", "теряй", "времени,", "выбирай", 
                              "слова", "и", "не", "упускай", "возможности"};
+//Задаём условие
 int lengthElement = 3;
+
 if (FindElements(inputArray, lengthElement))
 {
-    PrintArray(CreateSortArray(inputArray, lengthElement));
+    PrintArray(inputArray); //вывод заданного массива в консоль
+    Console.WriteLine();
+    PrintArray(CreateNewArray(inputArray, lengthElement));
 }
 else
+{
+    PrintArray(inputArray);
+    Console.WriteLine();
     Console.WriteLine("[]");
-
+}
